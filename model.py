aa7001e0842b44a5a6fd0bde10d59db1d454353c
KANABOON1 <1050970650@qq.com>
2025-04-22 23:08:08 +0800

    Naive GPT2 model.

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
 from dataclasses import dataclass
 import math
 import torch
@@ -6,12 +10,13 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256    # context size
-    vocab_size: int = 65     # vocabulary size
-    n_layer: int = 6         # number of layers
-    n_head: int = 6          # multi-head attention
-    n_embd: int = 384        # embedding dimension
+    block_size: int = 1024    # context size
+    vocab_size: int = 50257     # vocabulary size: 50000 BPE merges + 256 bytes tokens + 1 <|endoftxt|>
+    n_layer: int = 12         # number of layers
+    n_head: int = 12          # multi-head attention
+    n_embd: int = 768        # embedding dimension
     dropout: float = 0.0
+    bias: bool = False
 
 
 class MLP(nn.Module):
@@ -35,8 +40,6 @@ class CausalSelfAttention(nn.Module):
         
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
-        self.attn_dropout = nn.Dropout(config.attn_dropout)
-        self.resid_dropout = nn.Dropout(config.resid_dropout)
 
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -66,7 +69,6 @@ class CausalSelfAttention(nn.Module):
             # process attention scores
             att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
             att = F.softmax(att, dim=-1)  # note: 必须先softmax再做dropout, 否则dropout会打乱原有的权重分布
-            att = self.attn_dropout(att)
 
             # values
             y = att @ v
@@ -74,29 +76,25 @@ class CausalSelfAttention(nn.Module):
         y = y.transpose(1, 2).contiguous().view(B, T, C)
 
         # output projection
-        y = self.resid_dropout(self.c_proj(y))
+        y = self.c_proj(y)
 
-        return y
-
-
-
-            
+        return y            
 
 
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.ln1 = nn.LayerNorm(config.n_embd)   # 层归一化
+        self.ln_1 = nn.LayerNorm(config.n_embd)   # 层归一化
         self.attn = CausalSelfAttention(config)
-        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # NOTE - 相比于 `Attention is all you need`原论文中的block架构, 这里将residual产生的add与layer norm分离
         # 这样做的好处是: gradient从top -> bottom, 不会收到中间的layer norm的影响(梯度爆炸?)
         # 此时residual完全从模块中独立
-        x = x + self.attn(self.ln1(x))   # aggregation function (information collected between the tokens): communicate
-        x = x + self.mlp(self.ln2(x))    # mapping function: think individually
+        x = x + self.attn(self.ln_1(x))   # aggregation function (information collected between the tokens): communicate
+        x = x + self.mlp(self.ln_2(x))    # mapping function: think individually
 
         return x
 
@@ -110,6 +108,135 @@ class GPT(nn.Module):
             wte = nn.Embedding(config.vocab_size, config.n_embd),                # output embedding
             wpe = nn.Embedding(config.block_size, config.n_embd),                # position embedding
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),   # layers(blocks)
-            ln_f = nn.LayerNorm(config.n_embd),
+            ln_f = nn.LayerNorm(config.n_embd),                                  # layer_norm final
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+    
+    def forward(self, idx: torch.Tensor) -> torch.Tensor:
+        """GPT 根据输入的tokens预测下一个token
+
+        Args:
+            idx (torch.Tensor): 输入的 tokens, dim = [B, T]
+
+        Returns:
+            torch.Tensor: logits tensor, dim = [B, T, vocab_size], 每一个在输入sequence中的 token 都会计算出下一个token的概率
+        """
+        # process inputs
+        B, T = idx.size()   # batch size, sequence length
+        assert T < self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        # forward the token and position embeddings
+        # NOTE - 按照输入的 idx 所在设备处理, 如果 idx 设备与模型设备不一致, 则后续会报错
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)  
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+        x = pos_emb + tok_emb
+
+        # forward the blocks of transformers
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layer norm and classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+
+        return logits
+
+    @classmethod
+    def from_pretrained(cls, model_type, override_args=None):
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        override_args = override_args or {}
+        assert all(k == 'dropout' for k in override_args)
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+
+        print("forcing vocab_size=50257, block_size=1024, bias=True")
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        config_args['bias'] = True
+
+        if 'dropout' in override_args:
+            print(f"overriding dropout rate to {override_args['dropout']}")
+            config_args['dropout'] = override_args['dropout']
+        
+        # empty model 
+        model = GPT(GPTConfig(**config_args))
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
+        
+        sd_hf = GPT2LMHeadModel.from_pretrained(model_type).state_dict()
+        sd_keys_hf = [k for k in sd_hf.keys() if not k.endswith('.attn.bias')]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
+
+        transpose = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        assert len(sd_keys) == len(sd_keys_hf), f"mismatched keys: {len(sd_keys)} != {len(sd_keys_hf)}"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transpose):
+                # NOTE - transpose conv1D to linear
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
+    
+
+if __name__ == '__main__':
+    # hyperparameters
+    device = 'cpu'
+    if torch.cuda.available():
+        device = 'cuda'
+    elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+        device = 'mps'
+
+    num_return_sequences = 5
+    max_length = 30
+
+    # model = GPT.from_pretrained('gpt2')
+    model = GPT(GPTConfig())
+    model.eval()
+    model = model.to(device)
+
+    import tiktoken
+    enc = tiktoken.get_encoding("gpt2")
+    tokens = enc.encode("Hello, I'm a language model.")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+    x: torch.Tensor = tokens.to(device)
+    
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)
+    while x.size(1) < max_length:
+        with torch.no_grad():
+            logits = model(x)[:, -1, :]    # [B, vocab_size]
+            probs = F.softmax(logits, dim=-1)
+            
+            # topk sampling of 50 (huggingface pipeline 默认设置)
+            topk_probs, topk_indices = torch.topk(probs, k=50, dim=-1)   # [B, 50]
+            
+            # 从 samples 中按照概率随机抽取一个token
+            ix = torch.multinomial(topk_probs, num_samples=1)    # [B, 1]
+        
+            # gather the corresponding indices
+            xcol = torch.gather(topk_indices, dim=-1, index=ix)
+
+            # append to the sequence
+            x = torch.cat([x, xcol], dim=1)
+    
+    for i in range(num_return_sequences):
+        tokens = x[i][:max_length].tolist()
+        decoded = enc.decode(tokens)
+        print("> ", decoded)
+
